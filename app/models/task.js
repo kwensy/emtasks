38700d31f72a50938d2873e1eaddb279a3ef81bd
kwensy <theprophetbek@gmail.com>
2019-04-14 21:45:09 +0200

    Adds expected raw fields and one computed field to model

--- app/models/task.js
+++ app/models/task.js
@@ -2,5 +2,12 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
-
+		title : DS.attr('string'),
+		description : DS.attr('string'),
+		date : DS.attr('date'),
+		created : DS.attr('string', {
+			defaultValue: function(){
+				return new Date();
+			}
+		})
 });
