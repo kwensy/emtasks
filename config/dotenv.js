5aaff289a024c8bbc4fe54e6a6a99e5d81b1e9bb
kwensy <theprophetbek@gmail.com>
2019-05-01 21:24:06 +0200

    Secures sensitive environment variables

---
+++ config/dotenv.js
@@ -0,0 +1,9 @@
+module.exports = function(env) {
+  return {
+    clientAllowedKeys: [],
+    // Fail build when there is missing any of clientAllowedKeys environment variables.
+    // By default false.
+    failOnMissingKey: true,
+     path: '.env', 
+  };
+};
--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 'use strict';
-
+console.log(process.env);
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'emtasks',
@@ -8,13 +8,12 @@ module.exports = function(environment) {
     locationType: 'auto',
         
      firebase: {
-         apiKey: ""/*firebase apiKey*/,
-        authDomain: ""/*firebase authDomain*/,
-        databaseURL: ""/*firebase databaseUrl*/,
-        projectId: ""/*firebase projectId*/,
-        storageBucket: ""/*firebase storageBucket*/,
-        messagingSenderId: ""/*firebase messageSenderId*/
-
+         apiKey: process.env.FIREBASE_API_KEY,
+        authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId:  process.env.FIREBASE_PROJECT_ID,
+        storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
   },
     EmberENV: {
       FEATURES: {
