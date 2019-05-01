d55a634faaf55c0ba47fcdd5a85466766478093d
kwensy <theprophetbek@gmail.com>
2019-05-01 15:44:50 +0200

    Creates task edit form and add a link helper to it from tasks list items

--- app/routes/tasks/edit.js
+++ app/routes/tasks/edit.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { formatDate } from 'emtasks/helpers/format-date';
 
 export default Route.extend({
+	 setupController : function (controller, model) {
+		this._super(controller, model);
+		// prepare model date for controller to be displayed in template
+  		controller.set('displayedDate', formatDate([model.get('date')]));
+	}
+
 });
